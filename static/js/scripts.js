ba75de4cdfddac187e659c549906c68ca62a917b
Jan Tanis <jan@jantanis.net>
2022-11-28 12:34:10 +0100

    Changes

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -32,10 +32,11 @@ window.addEventListener('DOMContentLoaded', event => {
     // Activate Bootstrap scrollspy on the main nav element
     const mainNav = document.body.querySelector('#mainNav');
     if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
+
+        var scrollSpy = new bootstrap.ScrollSpy(document.body, {
             target: '#mainNav',
-            offset: 74,
-        });
+            offset: 74
+          })
     };
 
     // Collapse responsive navbar when toggler is visible
